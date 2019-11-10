7909bb4fd340dc0c6771f16a7fa58d5d07cf54fc
Eliezer Benjamin <eliezerben96@gmail.com>
2019-11-10 21:46:56 +0530

    Split method '_get_song_info' into two seperate methods 'get_track_info' and 'get_track_caption'

--- fra_kaare_sender.py
+++ fra_kaare_sender.py
@@ -46,14 +46,14 @@ class FraKaareSender:
     def send_new_tracks(self):
         last_sent_track_id = self.db_man.get_last_sent_track_id()
         print(f'Last sent track id: {last_sent_track_id}')
-        tracks_published_order = self._get_new_tracks()
+        tracks_published_order = self.get_new_tracks()
         tracks_sending_order = tracks_published_order[::-1]
         if tracks_sending_order:
             track_ids_to_send = [str(tr['id']) for tr in tracks_sending_order]
             print(f"Tracks to send: {', '.join(track_ids_to_send)}")
             for track in tracks_sending_order:
                 print(f"Sending track: {track['id']}", end='', flush=True)
-                send_success = self._send_track(track)
+                send_success = self.send_track(track)
                 if send_success:
                     print(' - Success')
                     self.db_man.set_last_sent_track_id(track['id'])
@@ -63,7 +63,7 @@ class FraKaareSender:
         else:
             print("No tracks to send")
 
-    def _get_new_tracks(self):
+    def get_new_tracks(self):
         """Return new tracks which have been published after the last track was sent"""
         new_tracks = []
         fra_kaare_tracks = self.bmm_api.podcastTracks(self.fra_kaare_podcast_id)
@@ -78,44 +78,95 @@ class FraKaareSender:
             new_tracks.extend(fra_kaare_tracks[:index])
         return new_tracks
 
-    def _get_song_info(self, track):
-        """Get information of song played in the track to be sent as caption"""
-        title = track['_meta'].get('title')
-        caption = f'<b>Title:</b> {title}'
+    def get_track_info(self, track):
+        """Get basic information about the track
+        Return:
+            {
+                'title': 'Track title',
+                'url': 'audio_url',
+                'song_book': '[HV|MB]',
+                'song_number': number
+            }
+        """
+        # Initial values
+        song_info = {
+            'title': None,
+            'url': None,
+            'song_book': None,
+            'song_number': None,
+        }
 
-        long_to_short = {
+        # Get title
+        song_info['title'] = track.get('title')
+
+        # Get audio url
+        song_url = files = None
+        media = track.get('media')
+        if media:
+            files = media[0].get('files')
+        if files:
+            song_url = files[0].get('url')
+        song_info['url'] = song_url
+
+        # Get song info
+        song_book_long_to_short = {
             'herrens_veier': 'HV',
             'mandelblomsten': 'MB',
         }
 
-        rel = None
-        if track['rel']:
-            rel = track['rel'][0]
+        rel_song = {}
+        if 'rel' in track and track['rel']:
+            rel_song = track['rel'][0]
+
+        song_book = rel_song.get('name')
+        song_number = rel_song.get('id')
+        if not (song_book and song_number):
+            return song_info
+
+        if song_book in song_book_long_to_short:
+            song_book = song_book_long_to_short[song_book]
         else:
-            return caption
+            song_book = song_book.replace('_', ' ').title()
 
-        rel_name = rel.get('name')
-        rel_id = rel.get('id')
-        if not rel_name or not rel_id:
-            return caption
+        song_info['song_book'] = song_book
+        song_info['song_number'] = song_number
 
-        if rel_name in long_to_short:
-            rel_name = long_to_short[rel_name]
-        else:
-            rel_name = rel_name.replace('_', ' ').title()
-        caption = f'{caption}\n<b>Song:</b> {rel_name} {rel_id}'
+        return song_info
 
-        return caption
-
-    def _send_track(self, track):
-        """Download and send track to telegram channel.
-        Return: True on success
-                False on failure
+    def get_track_caption(self, track_info):
+        """Get caption for the track
+        Return:
+            "<b>Title:</b> Track title here
+            <b>Song:</b> song_book_name song_number"
         """
-        track_url = track['media'][0]['files'][0]['url']
-        track_title = track['_meta'].get('title')
+        caption_lines = []
 
-        caption = self._get_song_info(track)
+        title = track_info['title']
+        if title:
+            caption_lines.append(f'<b>Title:</b> {title}')
+
+        song_book = track_info['song_book']
+        song_number = track_info['song_number']
+        if song_book:
+            caption_lines.append(f'<b>Song:</b> {song_book} {song_number}')
+
+        return '\n'.join(caption_lines)
+
+    def send_track(self, track):
+        """Download and send track to telegram channel.
+        Return:
+            False on error
+            True otherwise
+        """
+        track_info = self.get_track_info(track)
+        track_url = track_info['url']
+        track_title = track_info['title']
+
+        # There is no url. Silently skip sending this track.
+        if not track_url:
+            return True
+
+        caption = self.get_track_caption(track_info)
 
         try:
             response = self.bmm_api.get_response_object(track_url)
