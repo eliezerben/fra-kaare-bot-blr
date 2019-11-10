070481384fbbc4d0926d45796912b70835ebc41a
Eliezer Benjamin <eliezerben96@gmail.com>
2019-11-11 01:10:11 +0530

    Look for config file in script's directory instead of current directory

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+
 from fra_kaare_sender import FraKaareSender
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 if __name__ == '__main__':
-    sender = FraKaareSender(config='./config.json')
+    config_full_path = os.path.join(SCRIPT_DIR, 'config.json')
+    sender = FraKaareSender(config=config_full_path)
     sender.send_new_tracks()
